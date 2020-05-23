2c856267716d0aac23da0ccd9e51c65f4856ddb4
Ucandoit <yukan.zhao@gmail.com>
2020-05-23 14:45:17 +0200

    config: prepare for dockernize nobot-front

--- config-overrides.js
+++ config-overrides.js
@@ -2,14 +2,14 @@ const webpack = require('webpack');
 
 module.exports = function override(config, env) {
   // inject global variables for different profiles
-  if (env === 'development') {
-    config.plugins.push(new webpack.DefinePlugin({
-      ROOT_API: '"/."'
-    }));
-  } else if (env === 'production') {
-    config.plugins.push(new webpack.DefinePlugin({
-      ROOT_API: '"/nobot-backend"'
-    }));
-  }
+  config.plugins.push(new webpack.DefinePlugin({
+    ROOT_API: '"/."'
+  }));
+  // if (env === 'development') {
+  // } else if (env === 'production') {
+  //   config.plugins.push(new webpack.DefinePlugin({
+  //     ROOT_API: '"/nobot-backend"'
+  //   }));
+  // }
   return config;
 }
